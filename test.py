14ad7e3c8260895df0c4a230c8b3a1ac21a70376
Robert-Phan <fannk987@gmail.com>
2022-02-02 19:44:20 +0700

    FINALLY turned the repo into an actual project

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-from src.client import Client
+from src.ytwrapper.client import Client
+# TODO: Add in actual testing, instead of...whatever this is supposed to be.
 
 if __name__ == '__main__':
     yt = Client.from_client_secrets("client_secrets.json", ["https://www.googleapis.com/auth/youtube.force-ssl",
